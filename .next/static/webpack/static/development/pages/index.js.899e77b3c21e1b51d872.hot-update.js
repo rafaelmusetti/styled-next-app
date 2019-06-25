a423205ac5e8f9de9067bf8d70459b0350cd1c89
Rafael Musetti <musetti_rafael@hotmail.com>
2019-06-25 09:40:30 -0300

    first commit

---
+++ .next/static/webpack/static/development/pages/index.js.899e77b3c21e1b51d872.hot-update.js
@@ -0,0 +1,38 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./pages/index.js":
+/*!************************!*\
+  !*** ./pages/index.js ***!
+  \************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! styled-components */ "./node_modules/styled-components/dist/styled-components.browser.esm.js");
+var _jsxFileName = "/home/musetti/Desktop/workspace/styled-next-app/pages/index.js";
+
+
+var Title = styled_components__WEBPACK_IMPORTED_MODULE_1__["default"].h1.withConfig({
+  displayName: "pages__Title",
+  componentId: "sc-8pr893-0"
+})(["font-size:50px;color:", ";"], function (_ref) {
+  var theme = _ref.theme;
+  return theme.colors.primary;
+});
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(Title, {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 9
+    },
+    __self: this
+  }, "My page");
+});
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.899e77b3c21e1b51d872.hot-update.js.map
